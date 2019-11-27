b6ae121bc50037b0e8089c7cbfbf2d637663cac6
patreu22 <patrickreuter@outlook.com>
2019-11-27 17:10:16 +0100

    Improved Dev Setup

--- code/server.js
+++ code/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
+
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.static(path.join(__dirname, 'code-hub-frontend/build')));
 
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname + 'code-hub-frontend/build/index.html'));
 });
 
 app.listen(process.env.PORT || 5000, function () {
-    console.log("Serving on port 5000");
+    console.log(`Serving on port ${process.env.PORT || 5000}`);
 });
