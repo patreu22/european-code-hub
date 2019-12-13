f5dd8af745e85bde36257e2350a2c98a7647d437
patreu22 <patrickreuter@outlook.com>
2019-12-13 13:59:07 +0100

    Created httpRequest to fetch all projects

--- code/code-hub-frontend/src/helper/httpHelper.js
+++ code/code-hub-frontend/src/helper/httpHelper.js
@@ -4,7 +4,6 @@ import { getPasswordHash } from './passwordHelper';
 export function registerUser(username, password, mail, position) {
     console.log("Let's create a user!")
     const hash = getPasswordHash(password);
-
     axios.post('/api/create/user', {
         username: username,
         hash: hash,
@@ -28,4 +27,18 @@ export function registerProject({ gitUrl, projectName, projectDescription, conta
     }).catch(function (error) {
         console.log(error);
     });
+}
+
+export async function getAllProjects() {
+    const response = await axios.get('/api/get/projects')
+    //TODO: Remove sleep
+    await sleep(2000)
+    return new Promise(resolve => {
+        resolve({ projects: response.data })
+    });
+}
+
+//TODO: Remove sleep
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
