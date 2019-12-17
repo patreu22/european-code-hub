fd726564a5fc32caa9e50670118be162848de401
patreu22 <patrickreuter@outlook.com>
2019-12-17 17:05:25 +0100

    Added Modal to TU Logo

--- code/code-hub-frontend/src/components/Footer.js
+++ code/code-hub-frontend/src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core/';
 import tub_logo from '../assets/tub_logo.svg';
+import Modal from './Modal'
 
 
 const useStyles = makeStyles(theme => ({
@@ -20,16 +21,28 @@ const useStyles = makeStyles(theme => ({
     footerLogo: {
         position: 'fixed',
         bottom: 10,
-        right: 10
+        right: 10,
+        cursor: 'pointer'
     }
 }));
 
 export default function Footer() {
     const classes = useStyles();
+    const [openInfoModal, setOpenInfoModal] = React.useState(false);
     return (
         <Box boxShadow={4} className={classes.footer}>
             <span className={classes.footerNote}>EU ♡ FLOSS</span>
-            <img className={classes.footerLogo} height="27" width="37" src={tub_logo} alt="logo" />
+            <img className={classes.footerLogo} height="27" width="37" src={tub_logo} alt="logo" onClick={() => _handleTuLogoClick(setOpenInfoModal)} />
+            <Modal open={openInfoModal} handleClose={() => _handleModalClose(setOpenInfoModal)} />
         </Box >
     );
+}
+
+function _handleModalClose(setOpenInfoModal) {
+    console.log("Close")
+    setOpenInfoModal(false)
+}
+
+function _handleTuLogoClick(setOpenInfoModal) {
+    setOpenInfoModal(true)
 }
