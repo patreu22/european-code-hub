2077c3d90a1610fb9d328ed9318f61c88ecdea90
patreu22 <patrickreuter@outlook.com>
2020-01-19 20:17:31 +0100

    Updated the ManuallyDialogue to work from scratch or with preset values

--- code/code-hub-frontend/src/components/addProjectDialogues/ECHManuallyDialogue.js
+++ code/code-hub-frontend/src/components/addProjectDialogues/ECHManuallyDialogue.js
@@ -5,8 +5,9 @@ import ECHPaper from '../ECHPaper'
 import ECHBackButton from './ECHBackButton'
 import ECHButton from '../ECHButton'
 import ECHTextfield from '../ECHTextfield'
+import ECHMultipleSelect from '../ECHMultipleSelect'
 import { objectExists } from '../../helper/objectHelper'
-import { isValidText } from '../../helper/validationHelper'
+import { isValidText, isValidUrl } from '../../helper/validationHelper'
 import { formParagraphStyle } from './dialogueStyles'
 
 import { updateProjectDataAttribute } from '../../slices/createProjectSlice'
@@ -16,7 +17,19 @@ class ECHManuallyDialogue extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            projectNameErrorMessage: ''
+            projectNameErrorMessage: '',
+            projectDescriptionErrorMessage: '',
+            organizationErrorMessage: '',
+            repoUrlErrorMessage: '',
+            versionErrorMessage: '',
+            statusErrorMessage: '',
+            contactNameErrorMessage: '',
+            contactEmailErrorMessage: '',
+            dateCreatedErrorMessage: '',
+            dateLastModifiedErrorMessage: '',
+            programmingLanguagesErrorMessage: '',
+            licensesErrorMessage: '',
+            selectedList: []
         }
     }
 
@@ -36,24 +49,124 @@ class ECHManuallyDialogue extends Component {
     }
 
     _renderAddProjectManuallyForm() {
+        const contactName = this.props.projectData.contact ? this.props.projectData.contact.name : null
+        const contactMail = this.props.projectData.contact ? this.props.projectData.contact.email : null
+
         return <form style={formParagraphStyle}>
+            <ECHMultipleSelect title="Demo Input" />
             {this._renderField({
                 label: "Project name",
+                jsonKey: "projectName",
                 value: this.props.projectData.projectName,
                 errorMessageName: "projectNameErrorMessage",
                 errorMessage: this.state.projectNameErrorMessage,
+                validationType: "text",
+            })}
+            {/*TODO: Transform into Textbox instead Textfield*/}
+            {this._renderField({
+                label: "Project description",
+                jsonKey: "projectDescription",
+                value: this.props.projectData.projectDescription,
+                errorMessageName: "projectDescriptionErrorMessage",
+                errorMessage: this.state.projectDescriptionErrorMessage,
+                validationType: "text",
+            })}
+            {this._renderField({
+                label: "Organization",
+                jsonKey: "organization",
+                value: this.props.projectData.organization,
+                errorMessageName: "organizationErrorMessage",
+                errorMessage: this.state.organizationErrorMessage,
+                validationType: "text",
+            })}
+            {this._renderField({
+                label: "Repository URL",
+                jsonKey: "repoUrl",
+                value: this.props.projectData.repoUrl,
+                errorMessageName: "repoUrlErrorMessage",
+                errorMessage: this.state.repoUrlErrorMessage,
+                validationType: "url",
+            })}
+            {this._renderField({
+                label: "Version",
+                jsonKey: "version",
+                value: this.props.projectData.version,
+                errorMessageName: "versionErrorMessage",
+                errorMessage: this.state.versionErrorMessage,
+                validationType: "text",
+            })}
+            {/*TODO: Dropdown*/}
+            {this._renderField({
+                label: "Status",
+                jsonKey: "status",
+                value: this.props.projectData.status,
+                errorMessageName: "statusErrorMessage",
+                errorMessage: this.state.statusErrorMessage,
+                validationType: "text",
+            })}
+            {this._renderField({
+                label: "Contact name",
+                jsonKey: "contact.name",
+                value: contactName,
+                errorMessageName: "contactNameErrorMessage",
+                errorMessage: this.state.contactNameErrorMessage,
+                validationType: "text",
+            })}
+            {this._renderField({
+                label: "Contact email",
+                jsonKey: "contact.email",
+                value: contactMail,
+                errorMessageName: "contactEmailErrorMessage",
+                errorMessage: this.state.contactEmailErrorMessage,
+                validationType: "text",
+            })}
+            {/*TODO: Date Picker*/}
+            {this._renderField({
+                label: "Date created",
+                jsonKey: "dateCreated",
+                value: this.props.projectData.dateCreated,
+                errorMessageName: "dateCreatedErrorMessage",
+                errorMessage: this.state.dateCreatedErrorMessage,
+                validationType: "text",
+            })}
+            {/*TODO: Date Picker*/}
+            {this._renderField({
+                label: "Date of last modification",
+                jsonKey: "dateLastModified",
+                value: this.props.projectData.dateLastModified,
+                errorMessageName: "dateLastModifiedErrorMessage",
+                errorMessage: this.state.dateLastModifiedErrorMessage,
+                validationType: "text",
+            })}
+            {/*TODO: Multiselect Dropdown*/}
+            {this._renderField({
+                label: "Programming languages",
+                jsonKey: "programmingLanguages",
+                value: this.props.projectData.programmingLanguages,
+                errorMessageName: "programmingLanguagesErrorMessage",
+                errorMessage: this.state.programmingLanguagesErrorMessage,
+                validationType: "text",
+            })}
+            {/*TODO: Multiselect Dropdown*/}
+            {this._renderField({
+                label: "Licenses",
+                jsonKey: "licenses",
+                value: this.props.projectData.licenses,
+                errorMessageName: "licensesErrorMessage",
+                errorMessage: this.state.licensesErrorMessage,
                 validationType: "text",
             })}
             {this._renderSubmitButton()}
         </form>
     }
 
-    _renderField({ label, value, errorMessageName, errorMessage, validationType }) {
+
+    _renderField({ label, jsonKey, value, errorMessageName, errorMessage, validationType }) {
         const showError = errorMessage ? true : false
         return <ECHTextfield
             label={label}
             value={value ?? ""}
-            onChange={(event) => this.onTextfieldChanged(event)}
+            onChange={(event) => this.onTextfieldChanged(jsonKey, event)}
             onBlur={(event) => this.onTextfieldBlurred(errorMessageName, validationType, event)}
             error={showError}
             helperText={errorMessage}
@@ -66,11 +179,16 @@ class ECHManuallyDialogue extends Component {
             this.setState({
                 [errorMessageName]: isValid ? "" : "Invalid text."
             })
+        } else if (validationType === 'url') {
+            const isValid = isValidUrl(event.target.value)
+            this.setState({
+                [errorMessageName]: isValid ? "" : "Invalid URL."
+            })
         }
     }
 
-    onTextfieldChanged(event) {
-        this.props.updateProjectDataAttribute({ key: "projectName", value: event.target.value })
+    onTextfieldChanged(jsonKey, event) {
+        this.props.updateProjectDataAttribute({ key: jsonKey, value: event.target.value })
     }
 
     performManuallyHandling() {
