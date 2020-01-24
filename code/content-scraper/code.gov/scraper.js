54a5f9edf67ac28db99ca211328d66b36e47c115
patreu22 <patrickreuter@outlook.com>
2020-01-24 14:22:56 +0100

    Working approach for Code.gov Scan

--- code/content-scraper/code.gov/scraper.js
+++ code/content-scraper/code.gov/scraper.js
@@ -6,9 +6,6 @@ require('dotenv').config()
 const BASE_URL = "https://api.code.gov/repos";
 const API_KEY = process.env.CODE_GOV_API_KEY;
 
-const MONGOOSE_DB_URL = 'mongodb://localhost:27017/code-hub';
-const CHECKMARK_UNICODE = "\u2713";
-
 
 function main() {
     scrapeCodeGov()
@@ -17,17 +14,54 @@ function main() {
 function scrapeCodeGov() {
     const options = {
         method: 'GET',
-        url: "https://api.code.gov/repos",
+        url: BASE_URL,
         params: {
             api_key: API_KEY,
-            size: 10000
+            size: 10,
+            // size: 10000
         }
     }
 
     axios(options)
         .then((response) => {
-            console.log(response.data)
+            const repos = response.data.repos;
+            repos.forEach(repo => {
+                //TODO do an API call
+                registerProject({ projectData: transformRepoToProjectData(repo) })
+            });
         })
+}
+
+function transformRepoToProjectData(repo) {
+    var projectData = {
+        projectName: repo.name,
+        projectDescription: repo.description,
+        organization: repo.organization,
+        contact: {
+            name: repo.contact.name,
+            email: repo.contact.email,
+        },
+        date: {
+            created: repo.date.created,
+            lastModified: repo.date.lastModified
+        },
+        repoUrl: repo.repositoryURL,
+        programmingLanguages: repo.languages,
+        readme: "",
+        license: repo.permissions.licenses[0].name,
+        version: repo.version,
+        status: repo.status
+    }
+    return projectData
+}
+
+function registerProject({ projectData }) {
+    axios.post('http://localhost:5000/api/create/project', { projectData: projectData })
+        .then(function (response) {
+            // console.log(response);
+        }).catch(function (error) {
+            // console.log(error);
+        });
 }
 
 
