74111f5bd20cdcfa456df45807ed0ce24a315101
patreu22 <patrickreuter@outlook.com>
2020-03-25 01:33:27 +0100

    Added traeffic to docker-compose

--- code/ech-frontend/code-hub-frontend/src/App.js
+++ code/ech-frontend/code-hub-frontend/src/App.js
@@ -50,7 +50,7 @@ class App extends React.Component {
   }
 
   render() {
-    console.log("Test!")
+    console.log("Second Test!")
     return (
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <MuiThemeProvider theme={theme}>
--- code/ech-frontend/code-hub-frontend/src/helper/urlHelper.js
+++ code/ech-frontend/code-hub-frontend/src/helper/urlHelper.js
@@ -1,9 +1,10 @@
 const DEV_ENDPOINT = "http://localhost:5000";
-const PROD_ENDPOINT = "http://ech-backend:5000";
+const PROD_ENDPOINT = "http://ech-backend";
 
 export function getEndpoint(endpoint) {
     if (process.env.NODE_ENV === "production") {
         console.log("-Production lane-")
+        console.log("Endpoint: " + PROD_ENDPOINT + endpoint)
         return PROD_ENDPOINT + endpoint
     } else {
         console.log("-Dev lane-")
