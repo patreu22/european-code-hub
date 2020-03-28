9a45edfbb825035de33a9ea1ef089e2c74429ea7
patreu22 <patrickreuter@outlook.com>
2020-03-28 15:30:11 +0100

    Updated Load balancer endpoint

--- code/publish.sh
+++ code/publish.sh
@@ -1,12 +1,12 @@
-# docker login
-# cd ech-backend
-# docker build -t ech-backend:latest .
-# cd ../ech-frontend
-cd ech-frontend
+docker login
+cd ech-backend
+docker build -t ech-backend:latest .
+docker tag ech-backend patreu22/ech-backend:latest
+docker push patreu22/ech-backend:latest
+cd ../ech-frontend
 yarn build
 yarn deploy
-# docker build -t ech-frontend:latest .
-# docker tag ech-frontend patreu22/ech-frontend:latest
-# docker push patreu22/ech-frontend:latest
-# docker tag ech-backend patreu22/ech-backend:latest
-# docker push patreu22/ech-backend:latest
+docker build -t ech-frontend:latest .
+docker tag ech-frontend patreu22/ech-frontend:latest
+docker push patreu22/ech-frontend:latest
+
